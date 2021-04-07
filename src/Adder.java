b6293244185cfb033e45ebd4a94f7002083ecbd9
sysadmin <sysadmin@bcalab1-m03>
2021-04-07 08:42:01 +0530

    initial commit

---
+++ src/Adder.java
@@ -0,0 +1,15 @@
+
+public class Adder {
+	
+	int num1,num2;
+	Adder(int num1,int num2){
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
